bdfe66cda7d551d4462a4f6037003d15acf828f4
Nicolas Rozas <nicolas.rozas.la@gmail.com>
2022-02-28 17:55:35 -0300

    first commit

---
+++ pages/pokemones/[id].js
@@ -0,0 +1,51 @@
+import Image from "next/image";
+import Link from "next/link";
+import Router, { useRouter } from "next/router";
+
+const Pokemon = ({ data }) => {
+  const router = useRouter();
+  console.log(router);
+
+  if (router.isFallback) {
+    
+    return <p>Cargando...</p>
+  }
+  return (
+    <div>
+      <h1>
+        {data.name} Numero: #{data.id}
+      </h1>
+      <Image src={data.sprites.front_default} width={400} height={400} />
+      <Link href={"/"}>Volver al inicio</Link>
+    </div>
+  );
+};
+
+export default Pokemon;
+
+export const getStaticProps = async ({ params }) => {
+  const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${params.id}`);
+  const data = await res.json();
+
+  return {
+    props: { data },
+  };
+};
+
+export const getStaticPaths = async () => {
+  const paths = [{ params: { id: "1" } }, { params: { id: "2" } }];
+
+  return {
+    paths,
+    fallback: true,
+  };
+};
+
+// export const getServerSideProps = async ({ params }) => {
+//   const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${params.id}`);
+//   const data = await res.json();
+
+//   return {
+//     props: { data },
+//   };
+// };
